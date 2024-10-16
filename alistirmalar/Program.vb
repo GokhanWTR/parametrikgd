0db2e1df85014527dee8a8568bdd965e321553b2
GokhanWTR <sfc033@wittur.com>
2024-10-16 20:44:02 +0300

    ders sonu projesi yapıldı

--- alistirmalar/Program.vb
+++ alistirmalar/Program.vb
@@ -1,12 +1,46 @@
-Imports System
+' Module Module1
+ 
 
-Module Program
-    Sub Main(args As String())
-  Din yükseklik As Double = 12
-  Din taban As  Double = 5
-  Din alan 
+'     Sub DefineParts(ByRef parts As List(Of String))
+     
+'         parts.Add("Part1")
+'         parts.Add("Part2")
+'         parts.Add("Part3")
+'     End Sub
+ 
+  
+'     Sub ArrangeParts(parts As List(Of String))
+'         For Each part As String In parts
+           
+'             Console.WriteLine("Arranging: " & part)
+'         Next
+'     End Sub
+ 
 
-
+'     Sub ImportDFT(fileName As String)
+       
+'         Dim length As Integer = 600
+'         Dim width As Integer = 600
+ 
+'         Console.WriteLine("Imported DFT file: " & fileName)
+'         Console.WriteLine("Assuming dimensions: " & length & "mm x " & width & "mm")
+'     End Sub
+ 
+'     Sub Main()
+       
+'         Dim parts As New List(Of String)
+ 
+       
+'         DefineParts(parts)
+ 
+      
+'         ArrangeParts(parts)
+ 
         
-    End Sub
-End Module
+'         ImportDFT("1092672N.DFT")
+ 
+'         Console.WriteLine("Project completed.")
+'         Console.ReadLine() 
+'     End Sub
+ 
+' End Module
